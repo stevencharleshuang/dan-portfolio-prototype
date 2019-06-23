4f1233746a298af25fa731ffd36211e53343f439
Steven Huang <steven.charles.huang@gmail.com>
2019-06-23 01:51:41 -0400

    completed CV component, added styles

--- src/components/CV.jsx
+++ src/components/CV.jsx
@@ -3,7 +3,112 @@ import React from 'react';
 export default function CV() {
   return(
     <div className="cv">
-      <h1>This is the CV</h1>
+      <p className="left">
+        EDUCATION
+      </p>
+      <div className="education right">
+        <p>
+          MFA Tyler School of Art, Philadelphia, Pennsylvania, 2011
+        </p>
+        <p>
+          BFA SUNY Purchase College, Purchase, New York, 2008
+        </p>
+      </div>
+      <p className="left">
+        SOLO EXHIBITIONS
+      </p>
+      <div className="solo-exhibitions right">
+        <p>
+          Paintings of Horses and Other Things Also, Marvin Gardens, Ridgewood, NY, 2018
+        </p>
+        <p>
+          Naked Pictures, Galleri Tom Christoffersen, Copenhagen, Denmark, 2017
+        </p>
+        <p>
+          Where Do We Dump the Bodies, Mike Weiss Gallery, New York, NY, 2016
+        </p>
+        <p>
+          Four Paintings, Project Space, Galleri Tom Christoffersen, Copenhagen, Denmark, 2015
+        </p>
+        <p>
+          New Paintings, Galleri Brandstrup, Oslo, Norway, 2014
+        </p>
+        <p>
+          I Believe in Psychopaths, Galleri Tom Christoffersen, Copenhagen, Denmark, 2013
+        </p>
+        <p>
+          As You Stand There Watching, Galleri Tom Christoffersen, Copenhagen, Denmark, 2012
+        </p>
+      </div>
+      <p className="left">
+        GROUP EXHIBITIONS
+      </p>
+      <div className="group-exhibitions right">
+        <p>
+          Sieben, Ober Gallery, Kent, CT, 2018
+        </p>
+        <p>
+          Heart Break Hotel, Invisible Exports, New York, NY, 2017
+        </p>
+        <p>
+          Paper Peddling, Marvin Gardens, Ridgewood, NY, 2016
+        </p>
+        <p>
+          Teeth Gnash Tennessee, Invisible Exports, New York, NY, 2015 
+        </p>
+        <p>
+          Brask Collection at Munkeruphus, Hornbaek, Denmark, 2014
+        </p>
+        <p>
+          News From New York, Galleri Tom Christoffersen, Copenhagen, Denmark, 2013
+        </p>
+        <p>
+          It Happened in the Woods, Hans Alf Gallery, Copenhagen, Denmark, 2012
+        </p>
+      </div>
+      <p className="left">
+        WRITE-UPS
+      </p>
+      <div className="write-ups right">
+        <p>
+          <a href="https://artefuse.com/2018/11/29/dan-schein-at-marvin-gardens-125915/"  target="_blank" rel="noopener noreferrer">
+            "Dan Schein: Paintings of Horses and Other Things Also at Marvin Gardens"<br />
+            Arte Fuse November 29, 2018
+          </a>
+        </p>
+        <p>
+          <a href="https://openhouseblog.nyc/2016/10/10/dan-scheins-mountain-men-mock-the-macho/" target="_blank" rel="noopener noreferrer">
+            “Dan Schein’s Mountain Men Mock the Macho,”<br />
+            Open House October 10, 2016
+          </a>
+        </p>
+        <p>
+          <a href="https://politiken.dk/debat/kritikerskolen/art5474554/To-udstillinger-der-kan-g%C3%B8re-din-weekend-lidt-mindre-komfortabel" target="_blank" rel="noopener noreferrer">
+            “Two ExhibitionsThat Can Make Your Weekend a Little Less Comfortable,”<br />
+            Politiken October 7, 2013
+          </a>
+        </p>
+        <p>
+          <a href="https://tomchristoffersen.dk/artists/dan-schein/" target="_blank" rel="noopener noreferrer">
+            “Galleri Tom Chrisoffersen: Dan Schein: I Believe in Psychopaths,”<br />
+            re-title.com September 27, 2013
+          </a>
+        </p>
+        <p>
+          “Collector’s Edition: Jens-Peter Brask,”<br />
+          theworldsbestever.com September 24, 2013
+        </p>
+        <p>
+          <a href="http://www.christianstadil.com/inspiration/dan-schein">
+            Christian Stadil
+          </a>
+        </p>
+        <p>
+          <a href="https://stormfashion.dk/product/books/dan-schein" target="_blank" rel="noopener noreferrer">
+            stomfashion.dk
+          </a>
+        </p>
+      </div>
     </div>
   );
 }

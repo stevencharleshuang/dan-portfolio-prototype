77a173ac22aff8ab448160c5db8882dfab51cd6a
Steven Huang <steven.charles.huang@gmail.com>
2019-06-23 23:18:52 -0400

    implemented modal for paintings, refactored paintings component to class, added modal component, updated readme sources

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 ### Dan Schein Portfolio Prototype
+
+## Sources
+* [Modal Pattern ](https://alligator.io/react/modal-component/)
+* [No Select CSS Scripts ](https://stackoverflow.com/questions/826782/how-to-disable-text-selection-highlighting) 
--- src/components/Paintings.jsx
+++ src/components/Paintings.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Modal from './Modal';
 import P000 from '../img/paintings-000.jpg';
 import P001 from '../img/paintings-001.jpg';
 import P002 from '../img/paintings-002.jpg';
@@ -9,23 +10,56 @@ import P006 from '../img/paintings-006.jpg';
 import P007 from '../img/paintings-007.jpg';
 import P008 from '../img/paintings-008.jpg';
 
-export default function Paintings() {
-  let paintingsArr = [
-    P000,
-    P001,
-    P002,
-    P003,
-    P004,
-    P005,
-    P006,
-    P007,
-    P008
-  ];
-  let paintings = paintingsArr.map((painting, i) => <img src={painting} alt="painting" key={i} />);
+export default class Paintings extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: false
+    };
+    this.showModal = this.showModal.bind(this); 
+    this.hideModal = this.hideModal.bind(this); 
+  };
 
-  return(
-    <div className="paintings">
-      {paintings}
-    </div>
-  );
+  showModal = () => {
+    this.setState({ show: true });
+  };
+
+  hideModal = () => {
+    this.setState({ show: false });
+  };
+
+  render() {
+    console.log(this.state);
+    const paintingsArr = [
+      P000,
+      P001,
+      P002,
+      P003,
+      P004,
+      P005,
+      P006,
+      P007,
+      P008
+    ];
+
+    let paintings = paintingsArr.map((painting, i) => {
+    return (
+      <img 
+        src={painting} 
+        alt="painting"
+        onClick={this.showModal} 
+        key={i} />
+      );
+    });
+    
+    return(
+      <div className="paintings">
+        {paintings}
+        <Modal show={this.state.show} handleClose={this.hideModal}>
+          <p>Modal</p>
+          <p>Data</p>
+        </Modal>
+      </div>
+    );
+  };
 }

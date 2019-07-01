21c2dd8c290a7f1451489f665d2ebe9e74ba5bba
Steven Huang <steven.charles.huang@gmail.com>
2019-07-01 00:50:00 -0400

    implemented left and right button click handlers for modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -3,13 +3,27 @@ import React from 'react';
 export default function Modal({ handleClose, show, children }) {
   const showHideClassName = show ? "modal display-block" : "modal display-none";
 
+  const handleLBtnClick = (e) => {
+    console.log('Modal left button click!', e);
+  }
+
+  const handleRBtnClick = (e) => {
+    console.log('Modal right button click!', e);
+  }
+
   return (
     <div className={showHideClassName}>
       <section className="modal-main">
         {children}
         <button className="modal-btn modal-btn-close" onClick={handleClose}>x</button>
-        <button className="modal-btn modal-btn-right">{'>'}</button>
-        <button className="modal-btn modal-btn-left">{'<'}</button>
+        <button 
+          className="modal-btn modal-btn-left"
+          onClick = {handleLBtnClick}
+          >{'<'}</button>
+        <button 
+          className="modal-btn modal-btn-right"
+          onClick = {handleRBtnClick}
+          >{'>'}</button>
       </section>
     </div>
   );

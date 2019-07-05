38969761b31030433dfc25b875a4c87c9c52d4d2
Steven Huang <steven.charles.huang@gmail.com>
2019-07-04 22:10:26 -0400

    added normalize module, refactored css for consistency in code organization, added facebook and email links to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
-import IG from '../img/ig.jpg';
+import Ig from '../img/ig.png';
+import Email from '../img/email.png';
+import Fb from '../img/fb.png';
 
 export default function Footer() {
   return (
     <footer>
-      Dan Schein 2019
       <a 
         href="https://www.instagram.com/dan__schein/" 
         target="_blank" 
         rel="noopener noreferrer">
-        <img src={IG} alt="instagram" />
+        <img src={Ig} alt="instagram" />
+      </a>
+      <a 
+        href="https://www.facebook.com/dan.schein.5" 
+        target="_blank" 
+        rel="noopener noreferrer">
+        <img src={Fb} alt="facebook" />
+      </a>
+      <a href="mailto:danschein1212@gmail.com">
+        <img src={Email} alt="e-mail" />
       </a>
     </footer>
   );
